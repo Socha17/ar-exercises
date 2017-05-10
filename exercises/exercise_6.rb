3857f7220bb0447ac5f53f01973b43983a154af1
Socha17 <mattsocha11@gmail.com>
2017-05-10 18:02:54 +0000

    exercise 7

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,11 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "bob", last_name: "long", hourly_rate: 5)
+@store1.employees.create(first_name: "john", last_name: "short", hourly_rate: 10)
+
+@store2.employees.create(first_name: "nick", last_name: "car", hourly_rate: 20)
+@store2.employees.create(first_name: "andrew", last_name: "house", hourly_rate: 25)
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,11 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "enter a store name"
+store_name = gets.chomp
+
+
+
+@test = Store.create(name:"#{store_name}")
+
+puts @test.errors.full_messages
